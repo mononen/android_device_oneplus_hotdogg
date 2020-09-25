e873ee0750c501fd9f12f017328948bad3963601
Matt Filetto <matt.filetto@gmail.com>
2020-09-24 19:14:27 -0700

    vendor_prop: set correct default network for single SIM
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    Change-Id: I4390b8d6d8af6d746bd1ca57989facf309c951ca

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,6 +1,6 @@
 PRODUCT_PROPERTY_OVERRIDES +=  \
     persist.radio.multisim.config=ssss \
-    ro.telephony.default_network=33,22 \
+    ro.telephony.default_network=33 \
     persist.vendor.radio.5g_mode_pref=1 \
     ro.vendor.boot.project_name=19861 \
     rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
