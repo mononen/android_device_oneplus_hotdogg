e017ccc327349dfce8f5e47c6fe2c287eaff4270
Matt Filetto <matt.filetto@gmail.com>
2020-10-13 11:44:12 -0700

    vendorprops for 5g

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,7 +1,9 @@
 PRODUCT_PROPERTY_OVERRIDES +=  \
     persist.radio.multisim.config=ssss \
     persist.vendor.radio.5g_mode_pref=1 \
-    ro.telephony.default_network=33 \
+    ro.telephony.default_network=32,22 \
     ro.vendor.boot.project_name=19861 \
+    ro.boot.project_name=19861 \
     rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
-    vendor.boot.verifiedbootstate=orange
+    vendor.boot.verifiedbootstate=orange \
+    persist.vendor.radio.default_sa_enabled=1
