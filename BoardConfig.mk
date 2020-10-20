886bb59820e240c1bfc3f216a3f696b99cefb435
Matt Filetto <matt.filetto@gmail.com>
2020-10-20 01:58:26 -0700

    use weeb kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,7 +11,7 @@ DEVICE_PATH := device/oneplus/hotdogg
 # Assert
 TARGET_OTA_ASSERT_DEVICE := OnePlus7TProNR
 
-TARGET_KERNEL_CONFIG := vendor/sm8150-perf_defconfig
+TARGET_KERNEL_CONFIG := weeb_defconfig
 
 # Display
 TARGET_SCREEN_DENSITY := 560
