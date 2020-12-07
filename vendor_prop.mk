977c41428dab2b00ed3a292b3fac59b00e951d2a
Matt Filetto <matt.filetto@gmail.com>
2020-12-06 21:51:43 -0800

    hotdogg: add missing "project_name" build prop
    * Sometimes, OP kernels check for this
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -3,5 +3,6 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
     persist.vendor.radio.5g_mode_pref=1 \
     ro.telephony.default_network=33 \
     ro.vendor.boot.project_name=19861 \
+    ro.boot.project_name=19861 \
     rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
     vendor.boot.fingerprintbstate=orange
